02f76ba3f290668ab6b66181ec80bf89fe65637d
gigumay <gigumay@gmail.com>
2024-05-19 09:32:41 +0200

    Small bugfixes

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -470,7 +470,7 @@ class v8LocalizationLoss:
         h = model.args
         m = model.model[-1]  # Locate() module
         self.bce = nn.BCEWithLogitsLoss(reduction="none")
-        self.loc_loss = MSELoss()
+        self.loc_loss = HausdorffLoss()
         self.radii = model.radii
         self.hyp = h
         self.stride = m.stride
