fe779811f8790a68b6ec5ffcf714a61444a870a2
Tristan Walter <twalter@ab.mpg.de>
2026-01-24 20:46:20 +0100

    Refactor plotting logic: simplify confusion matrix plotting by removing redundant parameters

--- ultralytics/models/yolo/locate/val.py
+++ ultralytics/models/yolo/locate/val.py
@@ -202,9 +202,7 @@ class LocalizationValidator(BaseValidator):
 
         if self.args.plots:
             for normalize in True, False:
-                self.confusion_matrix.plot(
-                    save_dir=self.save_dir, names=self.names.values(), normalize=normalize, on_plot=self.on_plot
-                )
+                self.confusion_matrix.plot(save_dir=self.save_dir, normalize=normalize, on_plot=self.on_plot)
 
     def _process_batch(self, detections, gt_locations, gt_cls):
         """Return correct prediction matrix."""
